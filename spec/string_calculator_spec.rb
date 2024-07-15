8d00b2c753a960f2f22420af67021f82c08ebd78
amalsajesh441 <amalsajesh441@gmail.com>
2024-07-15 18:44:19 +0530

    String Calculator Test Case 1: Input Validation

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  it 'returns 0 for an empty string' do
+    expect(StringCalculator.add('')).to eq(0)
+  end
+
+  it 'returns the number itself for a single number' do
+    expect(StringCalculator.add('1')).to eq(1)
+  end
+
+  it 'returns the actual sum' do
+    expect(StringCalculator.add('1,5')).to eq(6)
+  end
+end
---
+++ string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+  def self.add(numbers)
+    numbers.split(',').map(&:to_i).sum
+  end
+end
